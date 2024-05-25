86b4ded020bd5cb146b28a5bb0a2c3e0b53eee06
shiridharKhatri <khatrishiridhar@gmail.com>
2024-05-25 17:59:39 +0545

    .

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
   const connectToDb = async (url) => {
     try {
-      let connection = await mongoose.connect(url);
+      let connection = await mongoose.connect(String(url));
       if (connection){
         console.log("Connected to mongodb database successfully")
       }
